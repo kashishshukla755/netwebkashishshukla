cbf0a18069f25a74b33f967fa8446bfae065bf13
kashish shukla <111456279+kashishshukla755@users.noreply.github.com>
2023-09-09 15:23:16 +0530

    done this the the working code

---
+++ src/Components/ProductPage.jsx
@@ -0,0 +1,15 @@
+// ProductPage.js
+import React from "react";
+import SearchInput from "./SearchInput";
+import ProductList from "./ProductList";
+
+function ProductPage() {
+  return (
+    <div>
+      <SearchInput />
+      <ProductList />
+    </div>
+  );
+}
+
+export default ProductPage;
